1d52d1dc6762b3f0c1e9081d7f0661738c638109
g0j <sergkuz92@gmail.com>
2021-11-06 17:23:04 +0300

    basic camera

--- main/main.c
+++ main/main.c
@@ -17,9 +17,9 @@
 #include "camera.h"
 
 
-static httpd_handle_t server = NULL;
+httpd_handle_t front_httpd = NULL;
 
-void init_misc()
+void initMisc()
 {
     //Initialize NVS
     esp_err_t err = nvs_flash_init();
@@ -33,52 +33,33 @@ void init_misc()
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 }
 
-void init_camera()
+void initCamera()
 {
     ESP_ERROR_CHECK(esp_camera_init(&camera_config));
     ESP_LOGE(CAMERA_TAG, "Camera Initialized");
 }
 
-void init_wifi()
+void initWifi()
 {
     ESP_LOGI(WIFI_TAG, "ESP_WIFI_MODE_STA");
     wifi_init_sta();
 }
 
-void init_server()
+void initServer()
 {
-    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &connect_handler, &server));
-    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &disconnect_handler, &server));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &connect_handler, &front_httpd));
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &disconnect_handler, &front_httpd));
 
-    /* Start the server for the first time */
-    server = start_webserver();
-    ESP_LOGI(HTTP_TAG, "ESP_WIFI_MODE_STA");
+    front_httpd = start_webserver();
+    ESP_LOGI(HTTP_TAG, "Web server initialized");
 
 }
 
 void app_main()
 {
-    gpio_pad_select_gpio(LED_LAMP);
-    gpio_set_direction(LED_LAMP, GPIO_MODE_OUTPUT);
-
-    init_misc();
-    init_wifi();
-    init_server();
-    init_camera();
-
-    camera_fb_t *pic;
-    while (1)
-    {
-        ESP_LOGI(CAMERA_TAG, "Taking picture...");
-        gpio_set_level(LED_LAMP, LED_ON);
-        vTaskDelay(500 / portTICK_RATE_MS);
-        pic = esp_camera_fb_get();
-
-        // use pic->buf to access the image
-        ESP_LOGI(CAMERA_TAG, "Picture taken! Its size was: %zu bytes", pic->len);
-        esp_camera_fb_return(pic);
-        gpio_set_level(LED_LAMP, LED_OFF);
-
-        vTaskDelay(5000 / portTICK_RATE_MS);
-    }
+    initLeds();
+    initMisc();
+    initWifi();
+    initCamera();
+    initServer();
 }
--- main/server.c
+++ main/server.c
@@ -11,6 +11,8 @@
 #include <esp_event.h>
 #include <esp_log.h>
 #include <esp_system.h>
+#include <esp_int_wdt.h>
+#include <esp_task_wdt.h>
 #include <nvs_flash.h>
 #include <sys/param.h>
 #include "nvs_flash.h"
@@ -20,6 +22,31 @@
 #include <esp_http_server.h>
 
 #include "server.h"
+#include "leds.h"
+
+#include "index_ov2640.h"
+#include "index_ov3660.h"
+#include "index_other.h"
+#include "css.h"
+#include "src/favicons.h"
+#include "src/logo.h"
+
+#include "esp_camera.h"
+
+int8_t streamCount = 0;          // Number of currently active streams
+unsigned long streamsServed = 0; // Total completed streams
+unsigned long imagesServed = 0;  // Total image requests
+
+typedef struct {
+        httpd_req_t *req;
+        size_t len;
+} jpg_chunking_t;
+
+#define PART_BOUNDARY "123456789000000000000987654321"
+static const char* _STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
+static const char* _STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
+static const char* _STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
+
 
 /* An HTTP GET handler */
 esp_err_t hello_get_handler(httpd_req_t *req)
@@ -132,6 +159,213 @@ esp_err_t http_404_error_handler(httpd_req_t *req, httpd_err_code_t err)
   return ESP_FAIL;
 }
 
+static esp_err_t index_handler(httpd_req_t *req)
+{
+  flashStatus(75);
+  ESP_LOGI(HTTP_TAG, "index page requested");
+  httpd_resp_set_type(req, "text/html");
+  httpd_resp_set_hdr(req, "Content-Encoding", "identity");
+  return httpd_resp_send(req, (const char *)index_simple_html, HTTPD_RESP_USE_STRLEN);
+}
+
+static esp_err_t style_handler(httpd_req_t *req)
+{
+  httpd_resp_set_type(req, "text/css");
+  httpd_resp_set_hdr(req, "Content-Encoding", "identity");
+  return httpd_resp_send(req, (const char *)style_css, style_css_len);
+}
+
+
+static esp_err_t streamviewer_handler(httpd_req_t *req){
+    flashStatus(75);
+    ESP_LOGI(HTTP_TAG,"Stream Viewer requested");
+    httpd_resp_set_type(req, "text/html");
+    httpd_resp_set_hdr(req, "Content-Encoding", "identity");
+    return httpd_resp_send(req, (const char *)streamviewer_html, streamviewer_html_len);
+}
+
+    
+void task_stream(void *param)
+{
+    httpd_req_t *req = (httpd_req_t *) param;
+    camera_fb_t * fb = NULL;
+    esp_err_t res = ESP_OK;
+    size_t _jpg_buf_len = 0;
+    uint8_t * _jpg_buf = NULL;
+    char * part_buf[64];
+
+    ESP_LOGI(HTTP_TAG,"Stream requested");
+    streamCount = 1;  // at present we only have one stream handler, so values are 0 or 1..
+    flashStatus(75);     // double flash of status LED
+    flashStatus(75);
+
+    static int64_t last_frame = 0;
+    if(!last_frame) {
+        last_frame = esp_timer_get_time();
+    }
+
+    res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
+    if(res != ESP_OK){
+        streamCount = 0;
+        ESP_LOGI(HTTP_TAG,"STREAM: failed to set HTTP response type");
+        vTaskDelete(NULL);
+        return;
+        // return res;
+    }
+
+    httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
+
+    while(true){
+        fb = esp_camera_fb_get();
+        if (!fb) {
+            ESP_LOGI(HTTP_TAG,"STREAM: failed to acquire frame");
+            res = ESP_FAIL;
+        } else {
+            if(fb->format != PIXFORMAT_JPEG){
+                ESP_LOGI(HTTP_TAG,"STREAM: Non-JPEG frame returned by camera module");
+                res = ESP_FAIL;
+            } else {
+                _jpg_buf_len = fb->len;
+                _jpg_buf = fb->buf;
+            }
+        }
+        if(res == ESP_OK){
+            res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
+        }
+        if(res == ESP_OK){
+            size_t hlen = snprintf((char *)part_buf, 64, _STREAM_PART, _jpg_buf_len);
+            res = httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
+        }
+        if(res == ESP_OK){
+            res = httpd_resp_send_chunk(req, (const char *)_jpg_buf, _jpg_buf_len);
+        }
+        if(fb){
+            esp_camera_fb_return(fb);
+            fb = NULL;
+            _jpg_buf = NULL;
+        } else if(_jpg_buf){
+            free(_jpg_buf);
+            _jpg_buf = NULL;
+        }
+        if(res != ESP_OK){
+            // This is the only exit point from the stream loop.
+            // We end the stream here only if a Hard failure has been encountered or the connection has been interrupted.
+            break;
+        }
+        int64_t frame_time = esp_timer_get_time() - last_frame;
+        last_frame = esp_timer_get_time();;
+        frame_time /= 1000;
+        ESP_LOGI(HTTP_TAG,"MJPG: %uB %ums (%.1ffps)\r\n",
+            (uint32_t)(_jpg_buf_len),
+            (uint32_t)frame_time, 1000.0 / (uint32_t)frame_time);
+    }
+
+    streamsServed++;
+    streamCount = 0;
+    ESP_LOGI(HTTP_TAG,"Stream ended");
+    last_frame = 0;
+    // return res;
+
+    vTaskDelete(NULL);
+}
+
+
+
+static esp_err_t stream_handler(httpd_req_t *req){
+  xTaskCreate(task_stream, "task_stream", 1024 * 4, req, 3, NULL);
+  return ESP_OK; // leave connection open
+}
+
+static esp_err_t cmd_handler(httpd_req_t *req){
+    char*  buf;
+    size_t buf_len;
+    char variable[32] = {0,};
+    char value[32] = {0,};
+
+    flashStatus(75);
+
+    buf_len = httpd_req_get_url_query_len(req) + 1;
+    if (buf_len > 1) {
+        buf = (char*)malloc(buf_len);
+        if(!buf){
+            httpd_resp_send_500(req);
+            return ESP_FAIL;
+        }
+        if (httpd_req_get_url_query_str(req, buf, buf_len) == ESP_OK) {
+            if (httpd_query_key_value(buf, "var", variable, sizeof(variable)) == ESP_OK &&
+                httpd_query_key_value(buf, "val", value, sizeof(value)) == ESP_OK) {
+            } else {
+                free(buf);
+                httpd_resp_send_404(req);
+                return ESP_FAIL;
+            }
+        } else {
+            free(buf);
+            httpd_resp_send_404(req);
+            return ESP_FAIL;
+        }
+        free(buf);
+    } else {
+        httpd_resp_send_404(req);
+        return ESP_FAIL;
+    }
+
+    int val = atoi(value);
+    sensor_t * s = esp_camera_sensor_get();
+    int res = 0;
+    if(!strcmp(variable, "framesize")) {
+        if(s->pixformat == PIXFORMAT_JPEG) res = s->set_framesize(s, (framesize_t)val);
+    }
+    else if(!strcmp(variable, "quality")) res = s->set_quality(s, val);
+    else if(!strcmp(variable, "contrast")) res = s->set_contrast(s, val);
+    else if(!strcmp(variable, "brightness")) res = s->set_brightness(s, val);
+    else if(!strcmp(variable, "saturation")) res = s->set_saturation(s, val);
+    else if(!strcmp(variable, "gainceiling")) res = s->set_gainceiling(s, (gainceiling_t)val);
+    else if(!strcmp(variable, "colorbar")) res = s->set_colorbar(s, val);
+    else if(!strcmp(variable, "awb")) res = s->set_whitebal(s, val);
+    else if(!strcmp(variable, "agc")) res = s->set_gain_ctrl(s, val);
+    else if(!strcmp(variable, "aec")) res = s->set_exposure_ctrl(s, val);
+    else if(!strcmp(variable, "hmirror")) res = s->set_hmirror(s, val);
+    else if(!strcmp(variable, "vflip")) res = s->set_vflip(s, val);
+    else if(!strcmp(variable, "awb_gain")) res = s->set_awb_gain(s, val);
+    else if(!strcmp(variable, "agc_gain")) res = s->set_agc_gain(s, val);
+    else if(!strcmp(variable, "aec_value")) res = s->set_aec_value(s, val);
+    else if(!strcmp(variable, "aec2")) res = s->set_aec2(s, val);
+    else if(!strcmp(variable, "dcw")) res = s->set_dcw(s, val);
+    else if(!strcmp(variable, "bpc")) res = s->set_bpc(s, val);
+    else if(!strcmp(variable, "wpc")) res = s->set_wpc(s, val);
+    else if(!strcmp(variable, "raw_gma")) res = s->set_raw_gma(s, val);
+    else if(!strcmp(variable, "lenc")) res = s->set_lenc(s, val);
+    else if(!strcmp(variable, "special_effect")) res = s->set_special_effect(s, val);
+    else if(!strcmp(variable, "wb_mode")) res = s->set_wb_mode(s, val);
+    else if(!strcmp(variable, "ae_level")) res = s->set_ae_level(s, val);
+    else if(!strcmp(variable, "lamp")) {
+        // lampVal = constrain(val,0,100);
+        setLamp(val);
+    }
+    else if(!strcmp(variable, "reboot")) {
+        esp_task_wdt_init(3,true);  // schedule a a watchdog panic event for 3 seconds in the future
+        esp_task_wdt_add(NULL);
+        periph_module_disable(PERIPH_I2C0_MODULE); // try to shut I2C down properly
+        periph_module_disable(PERIPH_I2C1_MODULE);
+        periph_module_reset(PERIPH_I2C0_MODULE);
+        periph_module_reset(PERIPH_I2C1_MODULE);
+        ESP_LOGI(HTTP_TAG,"REBOOT requested");
+        while(true) {
+          flashStatus(50);
+          vTaskDelay(150 / portTICK_PERIOD_MS);
+        }
+    }
+    else {
+        res = -1;
+    }
+    if(res){
+        return httpd_resp_send_500(req);
+    }
+    httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
+    return httpd_resp_send(req, NULL, 0);
+}
+
 httpd_handle_t start_webserver(void)
 {
   httpd_handle_t server = NULL;
@@ -143,19 +377,52 @@ httpd_handle_t start_webserver(void)
       .method = HTTP_GET,
       .handler = hello_get_handler,
       .user_ctx = "Hello World!"};
-
+  static const httpd_uri_t index_uri = {
+      .uri = "/",
+      .method = HTTP_GET,
+      .handler = index_handler,
+      .user_ctx = NULL};
+  static const httpd_uri_t style_uri = {
+      .uri = "/style.css",
+      .method = HTTP_GET,
+      .handler = style_handler,
+      .user_ctx = NULL};
+  static const httpd_uri_t stream_uri = {
+      .uri       = "/ws",
+      .method    = HTTP_GET,
+      .handler   = stream_handler,
+      .user_ctx  = NULL
+  };
+  static const httpd_uri_t streamviewer_uri = {
+      .uri       = "/view",
+      .method    = HTTP_GET,
+      .handler   = streamviewer_handler,
+      .user_ctx  = NULL
+  };
+  static const httpd_uri_t cmd_uri = {
+        .uri       = "/control",
+        .method    = HTTP_GET,
+        .handler   = cmd_handler,
+        .user_ctx  = NULL
+    };
   // Start the httpd server
   ESP_LOGI(HTTP_TAG, "Starting server on port: '%d'", config.server_port);
-  if (httpd_start(&server, &config) == ESP_OK)
+  if (httpd_start(&server, &config) != ESP_OK)
   {
-    // Set URI handlers
-    ESP_LOGI(HTTP_TAG, "Registering URI handlers");
-    httpd_register_uri_handler(server, &hello);
-    return server;
+    ESP_LOGI(HTTP_TAG, "Error starting server!");
+    return NULL;
   }
+  // Set URI handlers
+  ESP_LOGI(HTTP_TAG, "Registering URI handlers");
+  httpd_register_uri_handler(server, &hello);
+  httpd_register_uri_handler(server, &index_uri);
+  httpd_register_uri_handler(server, &style_uri);
+  httpd_register_uri_handler(server, &cmd_uri);
 
-  ESP_LOGI(HTTP_TAG, "Error starting server!");
-  return NULL;
+  httpd_register_uri_handler(server, &stream_uri);
+  httpd_register_uri_handler(server, &streamviewer_uri);
+
+  return server;
 }
 
 void stop_webserver(httpd_handle_t server)
@@ -165,7 +432,7 @@ void stop_webserver(httpd_handle_t server)
 }
 
 void disconnect_handler(void *arg, esp_event_base_t event_base,
-                               int32_t event_id, void *event_data)
+                        int32_t event_id, void *event_data)
 {
   httpd_handle_t *server = (httpd_handle_t *)arg;
   if (*server)
@@ -177,7 +444,7 @@ void disconnect_handler(void *arg, esp_event_base_t event_base,
 }
 
 void connect_handler(void *arg, esp_event_base_t event_base,
-                            int32_t event_id, void *event_data)
+                     int32_t event_id, void *event_data)
 {
   httpd_handle_t *server = (httpd_handle_t *)arg;
   if (*server == NULL)
